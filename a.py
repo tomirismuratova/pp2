bfb9d8c580e4040e9264a914edc6ef3fa875d82e
Tomiris <t_muratova@kbtu.kz>
2025-09-18 16:28:05 +0500

    tomiris

--- a.py
+++ a.py
@@ -1,1 +1,131 @@
-print("Tomi hello")
+#ex1
+print("Hello, World!")
+
+#SYNTAX
+if 5 > 2:
+  print("Five is greater than two!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+
+#VARIABLES
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+#You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+#You can also use the + operator to output multiple variables:
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+#For numbers, the + character works as a mathematical operator:
+x = 5
+y = 10
+print(x + y)
+
+#Unpack a list
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#Create a variable outside of a function, and use it inside the function
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#Create a variable inside a function, with the same name as the global variable
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+#use the global keyword if you want to change a global variable inside a function.
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+#To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+#Python numbers
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#To verify the type of any object in Python, use the type() function:
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#Convert from one type to another:
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+#convert from float to int:
+b = int(y)
+#convert from int to complex:
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Python casting
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
