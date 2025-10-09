11149633c903da7305a926138ebb13769a044e30
Tomiris <t_muratova@kbtu.kz>
2025-10-09 23:13:34 +0500

    Lab4

---
+++ Lab4/j.py
@@ -0,0 +1,21 @@
+import json
+
+with open("sample-data.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+items = data.get("imdata", [])
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':50} {'Description':20}  {'Speed':6}  {'MTU':6}")
+print(f"{'-'*50} {'-'*20}  {'-'*6}  {'-'*6}")
+
+for entry in items:
+    inner = next(iter(entry.values()))  
+    attrs = inner.get("attributes", {})
+
+    dn = attrs.get("dn", "")
+    descr = attrs.get("descr", "")
+    speed = attrs.get("speed", "") or attrs.get("operSpeed", "")
+    mtu = attrs.get("mtu", "")
+
+    print(f"{dn:50} {descr:20}  {str(speed):6}  {str(mtu):6}")
